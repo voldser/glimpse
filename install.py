0729d38ac46379d2ad89f5683d2fc75bd1da133f
voldser <59465471+voldser@users.noreply.github.com>
2021-11-05 01:38:38 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,29 +1,40 @@
 from subprocess import check_output
 import os
 from platform import system as systemos, architecture
-os.system('pip3 install -r requirements.txt')
+os.popen('pip3 install -r requirements.txt').read()
 from wget import download
 
-# NGROK
+def create_domain():
+        if True:
+                dir = os.popen('pwd').read()
+                dir2 = os.popen("whereis sh").read().split()[1][:-2] + "glimpse"
+                file = open(dir2, "w")
+                file.write("cd " + dir + "python3 main.py")
+                file.close()
+                os.system("chmod +x " + dir2)
+        else: pass
+
+
+
 def Ngrok():
-    if True:
-        if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
-            filename = 'ngrok-stable-linux-arm.zip'
-        else:
-            ostype = systemos().lower()
-            if architecture()[0] == '64bit':
-                filename = 'ngrok-stable-{0}-amd64.zip'.format(ostype)
-            else:
-                filename = 'ngrok-stable-{0}-386.zip'.format(ostype)
-        url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
-        download(url)
-        os.system('unzip ' + filename)
-        os.system('rm -Rf ' + filename)
-        os.system('clear')
+        if True:
+                if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
+                        filename = 'ngrok-stable-linux-arm.zip'
+                else:
+                        ostype = systemos().lower()
+                        if architecture()[0] == '64bit':
+                                filename = 'ngrok-stable-{0}-amd64.zip'.format(ostype)
+                        else:
+                                filename = 'ngrok-stable-{0}-386.zip'.format(ostype)
+                url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
+                download(url)
+                os.system('unzip ' + filename)
+                os.system('rm -Rf ' + filename)
+                os.system('clear')
 
 
 
 Ngrok()
-
-os.system('ssh-keygen -t ed25519')
+create_domain()
+os.system('ssh-keygen -t ed25519 -N ""')
 os.system('python3 main.py')
