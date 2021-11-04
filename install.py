c235407e2c96f91d6b32bb579e5d3b02e964532c
voldser <59465471+voldser@users.noreply.github.com>
2021-11-05 00:50:45 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,8 @@
 from subprocess import check_output
 import os
+import urllib.request
 from platform import system as systemos, architecture
-from wget import download
+
 
 # NGROK
 def Ngrok():
@@ -15,30 +16,14 @@ def Ngrok():
             else:
                 filename = 'ngrok-stable-{0}-386.zip'.format(ostype)
         url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
-        download(url)
+        urllib.request.urlretrieve(url)
         os.system('unzip ' + filename)
         os.system('rm -Rf ' + filename)
         os.system('clear')
 
 #LOCALXPOSE
-def Localxpose():
-    if True:
-        if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
-            filename = 'loclx-linux-arm.zip'
-        else:
-            ostype = systemos().lower()
-            if architecture()[0] == '64bit':
-                filename = 'loclx-linux-amd64.zip'.format(ostype)
-            else:
-                filename = 'loclx-linux-386.zip'.format(ostype)
-        url = 'https://lxpdownloads.sgp1.digitaloceanspaces.com/cli/'+filename
-        download(url)
-        os.system('unzip loclx*.zip')
-        os.system('rm loclx*.zip')
-        os.system("mv loclx* loclx")
-        os.system('clear')
 
 Ngrok()
-Localxpose()
+os.system('pip install -r requirements.txt')
 os.system('ssh-keygen -t ed25519')
-os.system('pip install -r requirements.txt && python main.py')
+os.system('python main.py')
